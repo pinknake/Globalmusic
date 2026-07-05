e26982a94c8e05c194891cab28ad6438b68619d2
Ankush Saggu <ankush22004@gmail.com>
2026-07-05 20:28:56 +0530

    Add new song to playlist with URL

--- songs.js
+++ songs.js
@@ -1,1 +1,8 @@
+playlist.push({
+  name: "#अरदस सतगर 2023  RadhaSwami Dinod Shabad  Paawan Charanon Mein  सतगर क चरण म अरदस",
+  file: null,
+  url: "https://pinknake.github.io/Globalmusic/#अरदस सतगर 2023  RadhaSwami Dinod Shabad  Paawan Charanon Mein  सतगर क चरण म अरदस"
+});
 
+renderSongs();
+loadSong(0);
