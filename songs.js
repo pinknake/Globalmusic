2589902e89fccf98e62c29b41ed6f2426208dc2a
Ankush Saggu <ankush22004@gmail.com>
2026-07-05 20:48:29 +0530

    Fix URL formatting in playlist entry

--- songs.js
+++ songs.js
@@ -1,8 +1,8 @@
 playlist.push({
   name: "#अरदस सतगर 2023  RadhaSwami Dinod Shabad  Paawan Charanon Mein  सतगर क चरण म अरदस",
   file: null,
-  url: "https://pinknake.github.io/Globalmusic/#अरदस सतगर 2023  RadhaSwami Dinod Shabad  Paawan Charanon Mein  सतगर क चरण म अरदस"
-});
+  url: "https://pinknake.github.io/Globalmusic/#%E0%A4%85%E0%A4%B0%E0%A4%A6%E0%A4%B8%20%E0%A4%B8%E0%A4%A4%E0%A4%97%E0%A4%B0%202023%20%20%20RadhaSwami%20Dinod%20Shabad%20%20Paawan%20Charanon%20Mein%20%E0%A4%B8%E0%A4%A4%E0%A4%97%E0%A4%B0%20%E0%A4%95%20%E0%A4%9A%E0%A4%B0%E0%A4%A3%20%E0%A4%AE%20%E0%A4%85%E0%A4%B0%E0%A4%A6%E0%A4%B8"
+    });
 
 renderSongs();
 loadSong(0);
