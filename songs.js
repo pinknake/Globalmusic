ec7dc59562b0c73ce20845b7d353c841ebe70c19
Ankush Saggu <ankush22004@gmail.com>
2026-07-05 21:00:19 +0530

    Modify playlist entries in songs.js
    Updated playlist with a new song and modified an existing entry.

--- songs.js
+++ songs.js
@@ -1,8 +1,16 @@
 playlist.push({
-  name: "#अरदस सतगर 2023  RadhaSwami Dinod Shabad  Paawan Charanon Mein  सतगर क चरण म अरदस",
+  name: "test",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/test"
     });
+
+renderSongs();
+loadSong(0);
+playlist.push({
+  name: "12 MANWA TU BANA MADARI",
+  file: null,
+  url: "https://pinknake.github.io/Globalmusic/12%20MANWA%20TU%20BANA%20MADARI"
+});
 
 renderSongs();
 loadSong(0);
