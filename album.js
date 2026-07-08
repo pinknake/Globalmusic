dc8453c623b865312922d46f9e7d47dfd0e6a0f6
Ankush Saggu <ankush22004@gmail.com>
2026-07-08 11:49:19 +0530

    Add album page button functionality

---
+++ album.js
@@ -0,0 +1,13 @@
+/ -------- Before playlist First song test.mp3 ------ /
+/ ------ ----- Alubum Page first ----------/ 
+  document
+.getElementById("albumPageBtn")
+.onclick = ()=>{
+
+pageTitle.textContent =
+"Albums";
+
+songList.innerHTML =
+"<div class='song'>Albums Coming Soon</div>";
+
+};
