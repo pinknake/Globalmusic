f617758ca15e86ec36fe8880cc74bbc42da5df9e
Ankush Saggu <ankush22004@gmail.com>
2026-07-09 20:15:46 +0530

    Add duration to songs in playlist

--- songs.js
+++ songs.js
@@ -2,7 +2,8 @@ playlist.push({
   name: "Ise Sant Ke Nyue Mile Sai Bhajan",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/IseSSantKKeNNyueMMileSSaiBBhajan",
-  cover:"radhaswamiji01.jpg"
+  cover:"radhaswamiji01.jpg",
+  duration:"05:33"
 });
 
 renderSongs();
@@ -12,7 +13,8 @@ playlist.push({
   name: "सुख दे दाता दुख दे दाता",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/सखददतदखददतRadhaSwamiShabadSantoKaDeshshab",
-cover:"radhaswamiji02.jpg"
+cover:"radhaswamiji02.jpg",
+  duration:"07:49"
 });
 
 renderSongs();
@@ -23,7 +25,8 @@ playlist.push({
   name: "Mein To Dhundat Dolun He Satguru",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/MeinTToDDhundatDDolunHHeSSatguru",
-cover:"radhaswamiji03.jpg"
+cover:"radhaswamiji03.jpg",
+  duration:"05:47"
 });
 
 renderSongs();
@@ -33,7 +36,8 @@ playlist.push({
   name: "Mere Satguru Deen Dayal Darshan",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/MereSatguruDeenDayalDarshan",
-cover:"radhaswamiji04.jpg"
+cover:"radhaswamiji04.jpg",
+  duration:"07:16"
 });
 
 renderSongs();
@@ -43,7 +47,8 @@ playlist.push({
   name: "भजन Prem ki Gehri Tu Misaal Hai सरलादास Satguru Bhajan",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/भजनPremkiGehriTuMisaalHaiसरलदसSatguruBh",
-cover:"radhaswamiji05.jpg"
+cover:"radhaswamiji05.jpg",
+  duration:"05:37"
 });
 
 renderSongs();
@@ -53,7 +58,8 @@ playlist.push({
   name: "Shukrana Guru Ji Aapka Din Raat",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/ShukranaGGuruJJiAAapkaDDinRRaat",
-cover:"radhaswamiji06.jpg"
+cover:"radhaswamiji06.jpg",
+  duration:"05:26"
 });
 
 renderSongs();
@@ -63,7 +69,8 @@ playlist.push({
   name: "Mann Lagyo Mero Yaar Fakiri Mein",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/MannLLagyoMMeroYYaarFFakiriMMein",
-  cover:"radhaswamiji07.jpg"
+  cover:"radhaswamiji07.jpg",
+  duration:"05:16"
 });
 
 renderSongs();
