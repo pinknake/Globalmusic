953fc63cd2b5f114eb9282ab2072db859f67afc3
Ankush Saggu <ankush22004@gmail.com>
2026-07-09 17:00:37 +0530

    Update song covers in playlist

--- songs.js
+++ songs.js
@@ -2,7 +2,8 @@ playlist.push({
   name: "Ise Sant Ke Nyue Mile Sai Bhajan",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/IseSSantKKeNNyueMMileSSaiBBhajan"
-    });
+  cover:"radhaswamiji01.jpg"
+});
 
 renderSongs();
 loadSong(0);
@@ -10,6 +11,7 @@ playlist.push({
   name: "सुख दे दाता दुख दे दाता",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/सखददतदखददतRadhaSwamiShabadSantoKaDeshshab"
+cover:"radhaswamiji02.jpg"
 });
 
 renderSongs();
@@ -20,6 +22,7 @@ playlist.push({
   name: "Mein To Dhundat Dolun He Satguru",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/MeinTToDDhundatDDolunHHeSSatguru"
+cover:"radhaswamiji03.jpg"
 });
 
 renderSongs();
@@ -29,6 +32,7 @@ playlist.push({
   name: "Mere Satguru Deen Dayal Darshan",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/MereSatguruDeenDayalDarshan"
+cover:"radhaswamiji04.jpg"
 });
 
 renderSongs();
@@ -38,6 +42,7 @@ playlist.push({
   name: "भजन Prem ki Gehri Tu Misaal Hai सरलादास Satguru Bhajan",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/भजनPremkiGehriTuMisaalHaiसरलदसSatguruBh"
+cover:"radhaswamiji05.jpg"
 });
 
 renderSongs();
@@ -47,6 +52,7 @@ playlist.push({
   name: "Shukrana Guru Ji Aapka Din Raat",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/ShukranaGGuruJJiAAapkaDDinRRaat"
+cover:"radhaswamiji06.jpg"
 });
 
 renderSongs();
@@ -56,7 +62,8 @@ playlist.push({
   name: "Mann Lagyo Mero Yaar Fakiri Mein",
   file: null,
   url: "https://pinknake.github.io/Globalmusic/MannLLagyoMMeroYYaarFFakiriMMein"
-    });
+  cover:"radhaswamiji07.jpg"
+});
 
 renderSongs();
 loadSong(0);
